46418fab535146814d346c14052d59cd4423de36
Praveen Kumar <cbkmar92@gmail.com>
2020-09-21 11:27:07 +0530

    add sidebar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from "./Sidebar"
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Chat from "./Chat"
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
